fbc99119973cd8a964276c5953b4e28ceba4714b
gtolbar <toledo970501@gmail.com>
2022-04-06 15:32:07 -0500

    Se anexa funcionalidad snackbar y mensaje

--- src/app/_service/informe.service.ts
+++ src/app/_service/informe.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { ConsultaResumenDTO } from '../_dto/consultaResumenDTO';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,10 @@ export class InformeService {
   constructor(
     private http:HttpClient,
   ) { }
+
+  listarResumen(fecha:string,fecha2:string){
+    return this.http.get<ConsultaResumenDTO[]>(`${this.url}/listar-resumen?fechaInicio=${fecha}&fechaFinal=${fecha2}`);
+  }
 
   generarReporte(fecha:string,fecha2:string){
     return this.http.get(`${this.url}/generarReporte?fechaInicio=${fecha}&fechaFinal=${fecha2}`,{
--- src/app/pages/informe/informe.component.ts
+++ src/app/pages/informe/informe.component.ts
@@ -3,6 +3,8 @@ import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import { DialogoInfomeComponent } from './dialogo-infome/dialogo-infome.component';
 import { InformeService } from 'src/app/_service/informe.service';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Chart, registerables } from 'chart.js';
+import * as moment from 'moment';
 
 
 @Component({
@@ -14,18 +16,24 @@ export class InformeComponent implements OnInit {
 
   form: FormGroup;
   maxFecha: Date = new Date();
-
+  chart: any;
 
   constructor(
     private informeService:InformeService,
-    public dialog: MatDialog
-  ) { }
+    public dialog: MatDialog,
+
+  ) {
+    Chart.register(...registerables);
+  }
 
   ngOnInit(): void {
+
     this.form = new FormGroup({
-      'fechaInicio': new FormControl(''),
-      'fechaFinal': new FormControl('')
+      'fechaInicio': new FormControl(moment(this.maxFecha).subtract(7,'day').format('YYYY-MM-DD')),
+      'fechaFinal': new FormControl(moment(this.maxFecha).format('YYYY-MM-DD'))
     });
+    this.generarCanvas();
+
   }
 
   openDialogInfo(){
@@ -34,4 +42,39 @@ export class InformeComponent implements OnInit {
     dialogConfig.height='90%';
     this.dialog.open(DialogoInfomeComponent,dialogConfig);
   }
+
+  prueba(){
+    this.chart.destroy();
+    this.generarCanvas();
+  }
+
+  generarCanvas(){
+    let fecha1 =this.form.value['fechaInicio'];
+    fecha1=fecha1 != "" ? moment(fecha1).format('YYYY-MM-DD') : '';
+    let fecha2 = this.form.value['fechaFinal'];
+    fecha2=fecha2 != "" ? moment(fecha2).format('YYYY-MM-DD') : '';
+    this.informeService.listarResumen(fecha1,fecha2).subscribe(data=>{
+      let cantidades= data.map(x=>x.cantidad);
+      let fechas= data.map(x=>x.fecha);
+      console.log(cantidades);
+      console.log(fechas);
+      this.chart = new Chart('canvas', {
+        type: 'line',
+        data: {
+          labels: fechas,
+          datasets: [
+            {
+              label: 'Cantidad',
+              data: cantidades,
+              borderColor: '#3cba9f',
+              fill: false,
+              tension: 0.1
+            }
+          ]
+        },
+      });
+
+    });
+  }
+
 }
