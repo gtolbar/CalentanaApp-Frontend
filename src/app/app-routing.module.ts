86af3c9d29eb2ee23b4fb543f6c83b276aef0d44
gtolbar <toledo970501@gmail.com>
2022-04-13 13:52:56 -0500

    Se anexa error 404 para pagina inexistentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { LoginComponent } from './pages/login/login.component';
+import { Not404Component } from './pages/not404/not404.component';
 import { GuardService } from './_service/guard.service';
 
 const routes: Routes = [
@@ -10,6 +11,10 @@ const routes: Routes = [
   { path: 'pages', component: LayoutComponent,
   loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule),
   canActivate: [GuardService]
+  },
+  { path: 'not-404', component: Not404Component},
+  {
+    path:'**',redirectTo:'not-404'
   }
 ];
 
